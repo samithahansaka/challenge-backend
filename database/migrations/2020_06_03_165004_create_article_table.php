08105637296300217a38322b5ac699376c7f35d9
Samitha Hansaka <samithahansaka@gmail.com>
2020-06-03 22:27:46 +0530

    Migrations created
    - migration files for author and article tables

---
+++ database/migrations/2020_06_03_165004_create_article_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('author');
+            $table->string('title', 255);
+            $table->string('url')->unique();
+            $table->text('content');
+            $table->dateTime('createdAt');
+            $table->dateTime('updatedAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
